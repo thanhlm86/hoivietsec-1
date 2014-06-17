63e1493bb5739938e58504df8078163934e0d93d
Thiem <tranthiem1993@gmail.com>
2014-06-17 14:19:45 +0700

    Delete NewsDelAction.php

--- Admin/NewsDelAction.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once("Class/ClassConnectDatabase.php");
-include_once("Class/ClassTableNews.php");
-
-$use1 = new Class_ConnectDatabase();
-$use2 = new Class_ClassTableNews();
-$use2->id = $_REQUEST['id'];
-$use2->Delete();
-header("Location: Index.php");
